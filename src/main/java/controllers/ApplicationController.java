4b88cc0be2b1b37c499d7d09e1e6918213ada589
baltazarO <ortizmob@oregonstate.edu>
2018-11-29 02:24:02 -0800

    Update

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -36,6 +36,7 @@ public class ApplicationController {
         if (result) {
             return Results.json().render(game);
         } else {
+            //error
             return Results.badRequest();
         }
     }
